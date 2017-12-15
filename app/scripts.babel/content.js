3f3b48f811b9e6efae855df45d180765e46827ac
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-12-15 11:23:07 +0100

    Bookmarking

---
+++ app/scripts.babel/content.js
@@ -0,0 +1,28 @@
+// Get post Id
+let postId =  document.querySelector('meta[name="twitter:app:url:iphone"]').content.replace('medium://p/', '');
+let isUpVoted =  false;
+
+var isBookmarked = document.querySelector('button[data-action-value="'+ postId +'"][data-action="remove-from-bookmarks"]')
+
+
+// TODO: Check if article is already voted
+console.log(`Article ${postId} is ${isBookmarked != null ? 'already' : 'not'} bookmarked`);
+
+function bookmark(){
+  if(!isBookmarked){
+    // click on button Bookmark
+    document.querySelector('button[data-action-value="'+ postId +'"][data-action="add-to-bookmarks"]').click();
+    isBookmarked =  true;
+  }
+}
+
+// Listen to clap button events
+var votes = document.querySelectorAll('button[data-action-value="'+ postId +'"][data-action="multivote"]');
+
+votes.forEach(function(button){
+  button.addEventListener('click', function(event) {
+    // Bookmark
+    bookmark()
+    // TODO: Add options page to know whether to bookmark also in browser (under which folder?)
+  });
+});
