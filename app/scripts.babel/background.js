7c1ff20c03210f68cad2fb42afbdb37431a613bc
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-12-18 13:52:29 +0100

    Update Google Analytics

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,7 +1,58 @@
 'use strict';
 
+const regexS = /medium.com\/.*/g;
+
 chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-console.log('\'Allo \'Allo! Event Page');
+
+// on tab updated
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    // code in here will run every time a user goes onto a new tab, so you can insert your scripts into every new tab
+
+    var regex = new RegExp(regexS);
+    var results = regex.exec(tab.url);
+
+    if(results){
+      // console.log(`Injecting Analytics in tab: ${tab.url}`);
+      // Init Google Analytics?
+      (function(i, s, o, g, r, a, m) {
+          i['GoogleAnalyticsObject'] = r;
+          i[r] = i[r] || function() {
+              (i[r].q = i[r].q || []).push(arguments)
+          }, i[r].l = 1 * new Date();
+          a = s.createElement(o),
+              m = s.getElementsByTagName(o)[0];
+          a.async = 1;
+          a.src = g;
+          m.parentNode.insertBefore(a, m)
+      })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga'); // Note: https protocol here
+
+      window.ga('create', 'UA-111375927-1', 'auto');
+      window.ga('set', 'checkProtocolTask', function() {}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+      window.ga('require', 'displayfeatures');
+    }
+
+});
+
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    // console.log('Received %o from %o, frame', msg, sender.tab, sender.frameId);
+
+    // tracking page views
+    if (msg.page) {
+        // track page
+        window.ga('send', 'pageview', msg.page);
+    }
+
+    // tracking events
+    if (msg.action) {
+        // track event
+        if (msg.value) {
+            window.ga('send', 'event', msg.category, msg.action, msg.label, msg.value);
+        } else {
+            window.ga('send', 'event', msg.category, msg.action);
+        }
+    }
+  });
--- app/scripts.babel/content.js
+++ app/scripts.babel/content.js
@@ -1,24 +1,75 @@
-// Get post Id
-let postId =  document.querySelector('meta[name="twitter:app:url:iphone"]').content.replace('medium://p/', '');
-let isUpVoted =  false;
-
-var isBookmarked = document.querySelector('button[data-action-value="'+ postId +'"][data-action="remove-from-bookmarks"]')
-console.log(`Article ${postId} is ${isBookmarked != null ? 'already' : 'not'} bookmarked`);
-
-function bookmark(){
-  if(!isBookmarked){
-    // click on button Bookmark
-    document.querySelector('button[data-action-value="'+ postId +'"][data-action="add-to-bookmarks"]').click();
-    isBookmarked =  true;
-  }
+/**
+ * Track Page with Google Analytics
+ * @param  {[type]} page [description]
+ * @return {[type]}      [description]
+ */
+function trackPage(page) {
+    chrome.runtime.sendMessage({
+        page: page
+    }, function(response) {
+        // console.log(response.farewell);
+    });
 }
 
-// Listen to clap button events
-var votes = document.querySelectorAll('button[data-action-value="'+ postId +'"][data-action="multivote"]');
+/**
+ * Track Event with Google Analytics
+ * @param  {[type]} category [description]
+ * @param  {[type]} event    [description]
+ * @param  {[type]} param    [description]
+ * @return {[type]}          [description]
+ */
+function trackEvent(category, action, label, value) {
+    chrome.runtime.sendMessage({
+        category: category,
+        action: action,
+        label: label,
+        value: value
+    }, function(response) {
+        // console.log(response.farewell);
+    });
+}
 
-votes.forEach(function(button){
-  button.addEventListener('click', function(event) {
-    bookmark()
-    // TODO: Add options page to know whether to bookmark also in browser (under which folder?)
+
+function isMediumArticle(){
+  let meta = document.querySelector('meta[name="twitter:app:url:iphone"]')
+  if (meta) {
+    var postId = meta.content.replace('medium://p/', '');
+    if(postId != ''){
+      // console.log(`${location.href} is a medium article !`)
+      return true;
+    }
+  }
+  return false;
+}
+
+
+if (isMediumArticle()) {
+  // Get post Id
+  let postId =  document.querySelector('meta[name="twitter:app:url:iphone"]').content.replace('medium://p/', '');
+  let isUpVoted =  false;
+
+  trackPage('/view')
+
+  var isBookmarked = document.querySelector('button[data-action-value="'+ postId +'"][data-action="remove-from-bookmarks"]')
+  // console.log(`Article ${postId} is ${isBookmarked != null ? 'already' : 'not'} bookmarked`);
+
+  function bookmark(){
+    trackEvent('Article', 'Clap', 'Clap', postId);
+    if(!isBookmarked){
+      // click on button Bookmark
+      document.querySelector('button[data-action-value="'+ postId +'"][data-action="add-to-bookmarks"]').click();
+      isBookmarked =  true;
+      trackEvent('Article', 'Bookmark', 'Bookmark', postId);
+    }
+  }
+
+  // Listen to clap button events
+  var votes = document.querySelectorAll('button[data-action-value="'+ postId +'"][data-action="multivote"]');
+
+  votes.forEach(function(button){
+    button.addEventListener('click', function(event) {
+      bookmark()
+      // TODO: Add options page to know whether to bookmark also in browser (under which folder?)
+    });
   });
-});
+}
